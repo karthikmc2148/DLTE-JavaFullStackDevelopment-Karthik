5e70278c12a1f84686c90ede3144da1c21950723
karthikmc2148 <karthikmc24aug2000@gmail.com>
2023-02-23 21:21:42 +0530

    sms banking

--- DLTE-JAVA/DLTE-java-001/src/main/java/assignment/SmsBanking.java
+++ DLTE-JAVA/DLTE-java-001/src/main/java/assignment/SmsBanking.java
@@ -5,17 +5,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Scanner;
+import java.util.SortedMap;
+
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 
 public class SmsBanking {
     public static void main(String[] args) {
-
+    Sms sms = new Sms();
+    sms.balanceEnqiry();
+    sms.recharge();
+    sms.bookTickets();
+    sms.services();
     }
 }
 
-@Data
+
 class Kyc{
     private Long accNum;
     private String name;
@@ -54,28 +59,69 @@ class Kyc{
         this.upi = upi;
     }
 }
-class Transation extends Kyc{
+class Transaction extends Kyc {
     Scanner scanner = new Scanner(System.in);
-    public void balanceEnqiry(){
+
+    public void balanceEnqiry() {
         System.out.println("Enter the account Holder name, account Number  :");
-            String name= scanner.nextLine();
-            Long accnum = scanner.nextLong();
-            setName(scanner.nextLine());
-            setAccNum(scanner.nextLong());
-            System.out.println("Enter the UPI ID:");
-            Integer upi=scanner.nextInt();
-            if(upi.equals(getUpi()))
-              System.out.println(name+" with "+accnum+"has the account balance Rs "+ getBalance()) ;
+        String name = scanner.nextLine();
+        Long accnum = scanner.nextLong();
+        setName(name);
+        setAccNum(accnum);
+        System.out.println("Enter the UPI ID:");
+        Integer upi = scanner.nextInt();
+        if (upi.equals(getUpi()))
+            System.out.println(name + " with Account Number  " + accnum + " has the account balance Rs " + getBalance());
+        else
+            System.out.println("Wrong UPI Entered!!");
+    }
+
+    public void recharge() {
+        System.out.println("Enter the Phone Number and amount to be recharged:");
+        Long phoneNo = scanner.nextLong();
+        Float amount = scanner.nextFloat();
+        System.out.println("Enter the UPI ID");
+        Integer upi = scanner.nextInt();
+        if (upi.equals(getUpi())) {
+
+            if (amount <= getBalance()) {
+                setBalance(getBalance() - amount);
+                System.out.println("Rs " + amount + " Recharge Successfull to mobile No: " + phoneNo);
+                System.out.println("Avaliable balance " + getBalance());
+            }
             else
-                System.out.println("Wrong UPI Entered!!")
+                System.out.println("Insufficient fund in your Account!!");
+        }
+        else System.out.println("wrong UPI ID entered!!");
     }
 
-    public void recharge(){
-        System.out.println(" this ");
-
-
+    public void bookTickets(){
+        System.out.println("Enter the Number of tickets:");
+                Integer tickets= scanner.nextInt();
+        Float amount = (float)(tickets * 100) ;//each ticket costs of Rs 100/-
+        System.out.println("Enter the UPI ID");
+        Integer upi = scanner.nextInt();
+        if (upi.equals(getUpi())) {
+            if (amount <= getBalance()) {
+                setBalance(getBalance() - amount);
+                System.out.println(tickets+" Tickets booked of Total amount "+amount);
+                System.out.println("Avaliable balance " + getBalance());
+            }
+            else System.out.println("Insufficient fund in your Account!!");
+        }
+       else System.out.println("wrong UPI ID entered!!");
     }
 }
-class banking{
+class Sms extends Transaction{
 
+    public void services() {
+        System.out.println("Enter 1901- for Balance Enqiry \n 2245- for Recharge");
+        Integer input =scanner.nextInt();
+        if(input==1901)
+            this.balanceEnqiry();
+        else if(input==2245)
+            this.recharge();
+        else System.out.println("Invalid Input!!");
+    }
 }
+
