4dfac2ab944968234fa208244ab875ea3b408ab0
karthikmc2148 <karthikmc24aug2000@gmail.com>
2023-02-27 17:58:27 +0530

    exception handles

--- DLTE-JAVA/DLITE-java-004/src/main/java/exceptions/handlings/ImplementationCheckedException.java
+++ DLTE-JAVA/DLITE-java-004/src/main/java/exceptions/handlings/ImplementationCheckedException.java
@@ -6,7 +6,7 @@ public class ImplementationCheckedException {
     public static void main(String[] args) throws IOException, InterruptedException {
         Runtime runtime=Runtime.getRuntime();
         Process process;
-        Thread.sleep(5000);
+
         process=runtime.exec("notepad");
         Thread.sleep(5000);
         process=runtime.exec("mspaint");
