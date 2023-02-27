c880242411c5a957a8330ec97d5ce428d0a5496b
karthikmc2148 <karthikmc24aug2000@gmail.com>
2023-02-27 18:18:19 +0530

    collections

--- DLTE-JAVA/DLITE-java-004/src/main/java/exceptions/handlings/ImplementationCheckedException.java
+++ DLTE-JAVA/DLITE-java-004/src/main/java/exceptions/handlings/ImplementationCheckedException.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 public class ImplementationCheckedException {
     public static void main(String[] args) throws IOException, InterruptedException {
         Runtime runtime=Runtime.getRuntime();
-        Process process;wwr
+        Process process;
 
 
         process=runtime.exec("notepad");
         Thread.sleep(5000);
         process=runtime.exec("mspaint");
-    }dsvd
+
+    }
 }
