3f5dda514e22e21333ddb20e82cde9b129981725
karthikmc2148 <karthikmc24aug2000@gmail.com>
2023-03-20 12:21:34 +0530

    Basic CRUDL Operations in JPA Repository

--- dlte-hibernate-jpa-repository/src/main/java/dlite/hibernate/dltehibernatejparepository/remote/TransactionRepository.java
+++ dlte-hibernate-jpa-repository/src/main/java/dlite/hibernate/dltehibernatejparepository/remote/TransactionRepository.java
@@ -13,9 +13,9 @@ public interface TransactionRepository extends CrudRepository<Transaction,Intege
     //  fetch List of Transaction by given sender
    List<Transaction> findAllBySender(String sender);
     //  fetch the List of Transaction by greater than given amount
-   @Query("from Transaction where amount>= : balance")
+   @Query("from Transaction where amount>= :balance")
     List<Transaction> findAllByAmount(float balance);
    // fetch the List of amounts matches the given type(Credit/debit)
-    @Query("select amount from Transaction where type = : kind")
+    @Query("select amount from Transaction where type = :kind")
     List<Float> findAllByType(String kind);
 }
