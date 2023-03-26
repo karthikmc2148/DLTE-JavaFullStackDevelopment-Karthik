62b11f7a8343510e28321fb70e4c1dfdcb003c78
karthikmc2148 <karthikmc24aug2000@gmail.com>
2023-03-27 01:14:38 +0530

    Student Login,fetch student Details, add new student

--- DLTE-java-review/src/main/java/review03/StudentInterface.java
+++
@@ -1,9 +0,0 @@
-package review03;
-
-import java.sql.SQLException;
-
-interface StudentInterface {
-    void insert(Student student,Address address ) throws SQLException;
-    void viewDetails() throws SQLException;
-    void delete( int reg_no) throws SQLException;
-}
--- DLTE-review-02-(StudentDetails-FileStorage)/src/main/java/dlte/review/App.java
+++ DLTE-review-02-(StudentDetails-FileStorage)/src/main/java/dlte/review/App.java
@@ -12,41 +12,58 @@ import static java.lang.System.exit;
  *
  */
 public class App { 
-    public static void main( String[] args ) throws IOException, ClassNotFoundException {
+    public static void main( String[] args ) throws IOException, ClassNotFoundException,CustomException {
         Scanner scanner  = new Scanner(System.in);
         Scanner scanner1  = new Scanner(System.in);
         StudentImplements studentImplements = new StudentImplements();
+        boolean success = true;
         do {
             System.out.println("----------------------------------Student Details----------------------------------");
             System.out.println("Select the operation to be performed: \n 1.Add New Student Details\n 2.Display All students details\n 3.Exit");
             switch (scanner.nextInt()) {
                 case 1:
                     Student student = new Student();
-                    Address address = new Address();
-                    System.out.println("Enter the student Name");
-                    student.setName(scanner1.nextLine());
+                    //Address address = new Address();
+
+                    while(success){
+                        try{
+                            System.out.println("Enter the student Name");
+                            student.setName(scanner1.nextLine());
+                            success = false;
+                        }
+                        catch(Exception e){
+                            System.out.println("Invalid input!!");
+                        }
+                    }
+
                     System.out.println("Enter the student Register Number");
                     student.setRegisterNumber(scanner.nextInt());
-                    System.out.println("Enter the student age");
-                    student.setAge(scanner.nextInt());
-                   System.out.println( "Enter the student EmailId");
+                    try {
+                        System.out.println("Enter the student age");
+                        student.setAge(scanner.nextInt());
+                        CustomException.validate(student.getAge());
+                    }
+                     catch(CustomException e){
+                        System.out.println(e);
+                     }
+                    System.out.println( "Enter the student EmailId");
                     student.setEmail(scanner1.nextLine());
                     System.out.println("Enter the Student addressDetails:");
                     System.out.println("Locality : ");
-                    address.setLocality(scanner1.nextLine());
+                    student.getAddress().setLocality(scanner1.nextLine());
                     System.out.println("Area : ");
-                    address.setArea(scanner1.nextLine());
+                    student.getAddress().setArea(scanner1.nextLine());
                     System.out.println("City : ");
-                    address.setCity(scanner1.nextLine());
+                    student.getAddress().setCity(scanner1.nextLine());
                     System.out.println("PIN Code : ");
-                    address.setPin(scanner.nextInt());
-                    student.setAddress(address);
+                    student.getAddress().setPin(scanner.nextInt());
+                    student.setAddress(student.getAddress());
                     studentImplements.studentList = studentImplements.readRecords();
                     studentImplements.studentList.add(student);
                     studentImplements.writeRecords(studentImplements.studentList);
                     break;
                 case 2:
-                    System.out.println("--------------------------------All Student Details-----------------------------------");
+                    System.out.println("--------------------------------All Students Information-----------------------------------");
                     ArrayList<Student> studentArrayList = studentImplements.readRecords();
                     int count = 1;
                     for (Student record : studentArrayList) {
